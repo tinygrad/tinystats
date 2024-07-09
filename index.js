d8aa62264dd207379f53711f5dc028032c546366
Woze Parrot <wozeparrot@gmail.com>
2024-07-09 14:51:10 -0700

    feat: show commit and run number on hover

--- index.js
+++ index.js
@@ -10,14 +10,15 @@ function main() {
   }
 
   // connect to websocket
-  const socket = new WebSocket("wss://tinymod.dev:10000");
-  // const socket = new WebSocket("ws://localhost:10000");
+  // const socket = new WebSocket("wss://tinymod.dev:10000");
+  const socket = new WebSocket("ws://localhost:10000");
   console.log("Connecting to websocket");
 
   // state
   const charts = {};
   let currCommit = "";
   let lastUpdateTime = Date.now();
+  let runCommitMap = {};
 
   function reload_charts() {
     for (const card of document.querySelectorAll(".stat-card")) {
@@ -94,18 +95,23 @@ function main() {
           Chartist.plugins.hoverline(),
         ],
       });
-    } else if ("commit" in data) {
+    } else if ("curr-commit" in data) {
       const lastUpdated = document.querySelector("#last-updated");
       lastUpdated.textContent = new Date(Date.now() - lastUpdateTime)
         .toISOString().slice(11, 19);
-      if (currCommit === data.commit) return;
+      if (currCommit === data["curr-commit"]) return;
       const commitElem = document.querySelector("#curr-commit");
-      commitElem.textContent = data.commit.slice(0, 7);
-      commitElem.href =
-        `https://github.com/tinygrad/tinygrad/commit/${data.commit}`;
-      currCommit = data.commit;
+      commitElem.textContent = data["curr-commit"].slice(0, 7);
+      commitElem.href = `https://github.com/tinygrad/tinygrad/commit/${
+        data["curr-commit"]
+      }`;
+      currCommit = data["curr-commit"];
 
       reload_charts();
+      socket.send("get-run-commit-map");
+    } else if ("run-commit-map" in data) {
+      runCommitMap = data["run-commit-map"];
+      console.log(runCommitMap);
     }
   };
 
@@ -147,124 +153,146 @@ function main() {
 
   socket.onopen = () => {
     console.log("Connected to websocket");
-    socket.send("get-commit");
+    socket.send("get-curr-commit");
     setInterval(() => {
-      socket.send("get-commit");
+      socket.send("get-curr-commit");
     }, 2000);
     for (const card of document.querySelectorAll(".stat-card")) {
       observer.observe(card);
     }
   };
+
+  (function (window, document, Chartist) {
+    "use strict";
+
+    Chartist.plugins = Chartist.plugins || {};
+    Chartist.plugins.hoverline = function () {
+      return function hoverline(chart) {
+        const $chart = chart.container;
+        let $lineIsShown = false;
+        let $lineFrozen = false;
+
+        let $line = $chart.querySelector(".chartist-hoverline");
+        if (!$line) {
+          $line = document.createElement("div");
+          $line.className = "chartist-hoverline";
+          const $box = document.querySelector(".hoverline-box");
+          $line.appendChild($box.cloneNode(true));
+          $chart.appendChild($line);
+        }
+
+        hide($line);
+
+        $chart.addEventListener("mouseover", function () {
+          show($line);
+        });
+
+        $chart.addEventListener("mouseout", function () {
+          if (!$lineFrozen) {
+            hide($line);
+          }
+        });
+
+        $chart.addEventListener("mousemove", function (event) {
+          if ($lineIsShown && !$lineFrozen) {
+            // locate the closest point on the x-axis
+            const eventX = event.layerX || event.offsetX;
+            let target, points;
+            if (chart instanceof Chartist.Line) {
+              target = event.target;
+              if (target.classList.contains("ct-point")) {
+                points = [target];
+              } else {
+                points = target.parentNode.querySelectorAll(".ct-point");
+              }
+            }
+            let closest;
+            if (points.length > 0) {
+              closest = Array.from(points).reduce((prev, curr) => {
+                if (!prev) return curr;
+                const prevDelta = Math.abs(prev.x1.baseVal.value - eventX);
+                const currDelta = Math.abs(curr.x1.baseVal.value - eventX);
+                return prevDelta < currDelta ? prev : curr;
+              });
+            }
+
+            setPosition(event, closest);
+          }
+        });
+
+        $chart.addEventListener("click", function () {
+          if ($lineFrozen) {
+            thaw($line);
+          } else {
+            freeze($line);
+          }
+        });
+
+        function setPosition(event, point) {
+          const width = $line.offsetWidth;
+          const offsetX = -width / 2;
+
+          const offsetBox = $chart.getBoundingClientRect();
+          const allOffsetLeft = -offsetBox.left - window.pageXOffset + offsetX;
+
+          if (point) {
+            const anchorLeft = point.x2.baseVal.value + offsetBox.left +
+              window.pageXOffset;
+            $line.style.left = anchorLeft + allOffsetLeft + "px";
+
+            const commitElem = $line.querySelector(".hoverline-commit");
+            const commit =
+              runCommitMap[point.getAttribute("ct:value").split(",")[0]].slice(
+                0,
+                7,
+              );
+            commitElem.textContent = commit;
+            commitElem.href =
+              `https://github.com/tinygrad/tinygrad/commit/${commit}`;
+
+            $line.querySelector(".hoverline-run").textContent =
+              point.getAttribute("ct:value").split(",")[0];
+          } else {
+            $line.style.left = event.pageX + allOffsetLeft + "px";
+            $line.querySelector(".hoverline-commit").textContent = "";
+            $line.querySelector(".hoverline-run").textContent = "";
+          }
+        }
+
+        function show(element) {
+          $lineIsShown = true;
+          if (!hasClass(element, "hoverline-show")) {
+            element.className = element.className + " hoverline-show";
+          }
+        }
+
+        function hide(element) {
+          $lineIsShown = false;
+          const regex = new RegExp("hoverline-show" + "\\s*", "gi");
+          element.className = element.className.replace(regex, "").trim();
+        }
+
+        function freeze(element) {
+          $lineFrozen = true;
+          if (!hasClass(element, "hoverline-frozen")) {
+            element.className = element.className + " hoverline-frozen";
+          }
+        }
+
+        function thaw(element) {
+          $lineFrozen = false;
+          const regex = new RegExp("hoverline-frozen" + "\\s*", "gi");
+          element.className = element.className.replace(regex, "").trim();
+        }
+      };
+    };
+
+    function hasClass(element, className) {
+      return (" " + element.getAttribute("class") + " ").indexOf(
+        " " + className + " ",
+      ) > -1;
+    }
+  })(window, document, Chartist);
 }
 
 main();
-
-// libs
-
-(function (window, document, Chartist) {
-  "use strict";
-
-  Chartist.plugins = Chartist.plugins || {};
-  Chartist.plugins.hoverline = function () {
-    return function hoverline(chart) {
-      const $chart = chart.container;
-      let $lineIsShown = false;
-
-      let $line = $chart.querySelector(".chartist-hoverline");
-      if (!$line) {
-        $line = document.createElement("div");
-        $line.className = "chartist-hoverline";
-        const $box = document.querySelector(".hoverline-box");
-        $line.appendChild($box.cloneNode(true));
-        $chart.appendChild($line);
-      }
-
-      hide($line);
-
-      $chart.addEventListener("mouseover", function () {
-        show($line);
-      });
-
-      $chart.addEventListener("mouseout", function () {
-        hide($line);
-      });
-
-      $chart.addEventListener("mousemove", function (event) {
-        if ($lineIsShown) {
-          // locate the closest point on the x-axis
-          const eventX = event.layerX || event.offsetX;
-          let target, points;
-          if (chart instanceof Chartist.Line) {
-            target = event.target;
-            if (target.classList.contains("ct-point")) {
-              points = [target];
-            } else {
-              points = target.parentNode.querySelectorAll(".ct-point");
-            }
-          }
-          let closest;
-          if (points.length > 0) {
-            closest = Array.from(points).reduce((prev, curr) => {
-              if (!prev) return curr;
-              const prevDelta = Math.abs(prev.x1.baseVal.value - eventX);
-              const currDelta = Math.abs(curr.x1.baseVal.value - eventX);
-              return prevDelta < currDelta ? prev : curr;
-            });
-          }
-
-          setPosition(event, closest);
-        }
-      });
-
-      function setPosition(event, point) {
-        const width = $line.offsetWidth;
-        const offsetX = -width / 2;
-
-        const offsetBox = $chart.getBoundingClientRect();
-        const allOffsetLeft = -offsetBox.left - window.pageXOffset + offsetX;
-
-        if (point) {
-          const anchorLeft = point.x2.baseVal.value + offsetBox.left + window.pageXOffset;
-          $line.style.left = anchorLeft + allOffsetLeft + "px";
-          $line.querySelector(".hoverline-run").textContent = point.getAttribute("ct:value").split(",")[0];
-        } else {
-          $line.style.left = event.pageX + allOffsetLeft + "px";
-          $line.querySelector(".hoverline-run").textContent = "";
-        }
-      }
-
-      /**
-       * Shows the line element, if not shown
-       * @param element
-       */
-      function show(element) {
-        $lineIsShown = true;
-        if (!hasClass(element, "hoverline-show")) {
-          element.className = element.className + " hoverline-show";
-        }
-      }
-
-      /**
-       * Hides the line element
-       * @param element
-       */
-      function hide(element) {
-        $lineIsShown = false;
-        const regex = new RegExp("hoverline-show" + "\\s*", "gi");
-        element.className = element.className.replace(regex, "").trim();
-      }
-    };
-  };
-
-  /**
-   * Returns whether a element has a css class called className
-   * @param element
-   * @param className
-   * @return {boolean}
-   */
-  function hasClass(element, className) {
-    return (" " + element.getAttribute("class") + " ").indexOf(
-      " " + className + " ",
-    ) > -1;
-  }
-})(window, document, Chartist);
